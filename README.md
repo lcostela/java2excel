2ca300d82c267fa601b4eb5750a0241a14f94093
Luis Costela <sergio_costela@hotmail.com>
2018-07-03 08:48:38 -0300

    Initial state of the project to refactor

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # java2excel
 [![Build Status](https://travis-ci.com/lcostela/java2excel.svg?branch=master)](https://travis-ci.com/lcostela/java2excel)
-
+[![License](https://img.shields.io/badge/license-MIT-green.svg)](https://github.com/lcostela/java2excel/blob/master/LICENSE)
 Library to convert Java classes with annotations to Excel file.
---
+++ src/main/java/io/github/lcostela/java2excel/java2excel/core/Java2ExcelConverter.java
@@ -0,0 +1,185 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2018 Luis Costela
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ */
+
+package io.github.lcostela.java2excel.java2excel.core;
+
+import io.github.lcostela.java2excel.java2excel.annotatios.CellHeader;
+import io.github.lcostela.java2excel.java2excel.annotatios.CellOrder;
+import io.github.lcostela.java2excel.java2excel.exception.IllegalTypeOfAttribute;
+import org.apache.commons.lang3.reflect.FieldUtils;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.util.CellRangeAddress;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.springframework.util.ReflectionUtils;
+
+import javax.persistence.Enumerated;
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.time.LocalDate;
+import java.util.List;
+
+/**
+ * Created by Luis Costela.
+ */
+public class Java2ExcelConverter<T> {
+
+  private Class<T> clazz;
+
+  public Java2ExcelConverter(Class<T> clazz) {
+    this.clazz = clazz;
+  }
+
+  private Class<T> obtainClass() {
+    return clazz;
+  }
+
+  public Workbook toWorkBook(List<T> itemReport, String name) throws IllegalTypeOfAttribute {
+    checkClassEligibleToConvert(obtainClass());
+    Workbook workbook = new XSSFWorkbook();
+    Sheet sheet = workbook.createSheet(name);
+    Row row = sheet.createRow(0);
+    generateHeaders(obtainClass(), row);
+    int i = 1;
+    for (T item : itemReport) {
+      row = sheet.createRow(i);
+      generateRow(item, row);
+      i++;
+    }
+    final int numberOfColumns = getNumberOfColumns(obtainClass());
+    autoSizeColumns(sheet, numberOfColumns);
+    sheet.setAutoFilter(new CellRangeAddress(0, i, 0, numberOfColumns));
+    return workbook;
+  }
+
+  private void checkClassEligibleToConvert(Class<T> clazz) throws IllegalTypeOfAttribute {
+    List<Field> attributes = FieldUtils.getAllFieldsList(clazz);
+    final Class[] classes = {Long.class, String.class, Double.class, LocalDate.class};
+    try {
+      compareClasses(attributes, classes);
+    } catch (IllegalTypeOfAttribute illegalTypeOfAttribute) {
+      //  illegalTypeOfAttribute.printStackTrace();
+      //TODO: log.err
+      throw illegalTypeOfAttribute;
+    }
+  }
+
+  private void compareClasses(List<Field> attributes, Class[] classes) throws IllegalTypeOfAttribute {
+    for (Field attribute : attributes) {
+      boolean cast = false;
+      for (Class klazz : classes) {
+        if (fieldIsFromClass(attribute, klazz)) {
+          cast = true;
+          break;
+        }
+      }
+      if (!cast && !attribute.isAnnotationPresent(Enumerated.class)) {
+        throw new IllegalTypeOfAttribute();
+      }
+    }
+  }
+
+  private boolean fieldIsFromClass(Field field, Class clazz) {
+    return field.getType().equals(clazz);
+  }
+
+  private int getNumberOfColumns(Class<T> item) {
+
+    int numberOfColumns = 0;
+    Method[] m = new Method[0];
+    try {
+      m = ReflectionUtils.getAllDeclaredMethods(Class.forName(item.newInstance().getClass().getName()));
+    } catch (ClassNotFoundException e) {
+      // todo log.err e.printStackTrace();
+    } catch (IllegalAccessException e) {
+      e.printStackTrace();
+    } catch (InstantiationException e) {
+      e.printStackTrace();
+    }
+    for (Method method : m) {
+      if (method.getAnnotation(CellOrder.class) != null) {
+        numberOfColumns++;
+      }
+    }
+    return numberOfColumns;
+  }
+
+  private void generateRow(T item, Row row) {
+    try {
+      Method[] m = ReflectionUtils.getAllDeclaredMethods(Class.forName(item.getClass().getName()));
+      for (Method method : m) {
+        if (method.getAnnotation(CellOrder.class) != null) {
+          int columnNumber = method.getAnnotation(CellOrder.class).value();
+          Cell cel = row.createCell(columnNumber);
+          Object value = method.invoke(item);
+          if (value != null) {
+            if (value instanceof String) {
+              cel.setCellValue((String) value);
+            } else if (value instanceof Long) {
+              cel.setCellValue((Long) value);
+            } else if (value instanceof Integer) {
+              cel.setCellValue((Integer) value);
+            } else if (value instanceof Double) {
+              cel.setCellValue((Double) value);
+            } else if (value instanceof LocalDate) {
+              cel.setCellValue(value.toString());
+            } else if (value instanceof Enum<?>) {
+              cel.setCellValue(((Enum) value).name());
+            }
+          }
+        }
+      }
+    } catch (IllegalAccessException | InvocationTargetException | ClassNotFoundException e) {
+      //todo log.err e.printStackTrace();
+    }
+  }
+
+  private void generateHeaders(Class<T> item, Row row) {
+    try {
+      Method[] m = ReflectionUtils.getAllDeclaredMethods(Class.forName(item.newInstance().getClass().getName()));
+      for (Method method : m) {
+        if (method.getAnnotation(CellOrder.class) != null) {
+          int columnNumber = method.getAnnotation(CellOrder.class).value();
+          row.createCell(columnNumber).setCellValue(method.getAnnotation(CellHeader.class).value());
+        }
+      }
+    } catch (ClassNotFoundException e) {
+      e.printStackTrace();
+    } catch (InstantiationException e) {
+      e.printStackTrace();
+    } catch (IllegalAccessException e) {
+      e.printStackTrace();
+    }
+  }
+
+  private void autoSizeColumns(Sheet sheet, int numberOfColumns) {
+    for (int i = 0; i < numberOfColumns; i++) {
+      sheet.autoSizeColumn(i);
+    }
+  }
+}
